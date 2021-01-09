5a43c09f8e667d47aa3d6c194444a5f8e952c8c6
Trev Morin <trev@superklok.com>
2021-01-08 20:27:24 -0600

    Add French Translation

---
+++ app.js
@@ -0,0 +1,24 @@
+if (process.env.NODE_ENV !== "production") {
+	require('dotenv').config();
+}
+
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+	res.render('home')
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Au service sur le port ${ port }`)
+});
---
+++ public/scripts/carousel.js
@@ -0,0 +1,6 @@
+$('.carousel').carousel({
+	interval: 10000,
+	keyboard: false,
+	touch: false,
+	pause: false
+});
