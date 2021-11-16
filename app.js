5588c29764671fa5ecacd85970a0ce3718af2b4e
Trev Morin <trev@superklok.com>
2021-11-15 18:35:47 -0600

    Add Restructure

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ if (process.env.NODE_ENV !== "production") {
 	require('dotenv').config();
 }
 
-const express = require('express');
-const path = require('path');
-const ejsMate = require('ejs-mate');
+const express = require('express'),
+	  path    = require('path'),
+	  ejsMate = require('ejs-mate'),
+	  app     = express();
 
-const app = express();
+app.engine('ejs', ejsMate);
 
-app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-	res.render('home')
+	res.render('home');
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Au service sur le port ${ port }`)
+	console.log(`Au service sur le port ${ port }`);
 });
